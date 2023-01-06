ccc883a98cc98c3fcf413ef99eef4fbdca64e81e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-01-06 10:46:55 +0530

    Added 10_Obj_primitive/

---
+++ 10_Obj_primitive/const.js
@@ -0,0 +1,60 @@
+// // const arr = 10;
+// const arr = [1, 2, 3, 4, 5];
+// // how it is possible ??
+// arr.shift
+// console.log(arr);
+
+
+// Objects
+// key: value
+// String, Number: validtype
+// let object = {
+//     name: "jashbir",
+//     lastName: "singh",
+//     sayHi: function (){
+//         console.log("jasbir say's hi");
+//     }
+// }
+// console.log("line number 16", object[25]);
+// console.log(object);
+
+// // loop
+// for(key in object){
+//     console.log(key, " : ", object[key]);
+// }
+// object.sayHi();
+// arrays
+// let arr = [1, 2, 3, 4, 5];
+// arr.myprop="Hello";
+// arr.print= function (){
+//     console.log("Hello from array");
+// }
+// for(key in arr){
+//     console.log(key, " : ", arr[key]);
+// }
+// console.log(arr);
+// arr[95]=100;
+// console.log(arr);
+// console.log(arr[23]);
+
+
+// function -> object -> key: value
+// extra feature -> code property that can be executed when you invoke that
+
+
+// function fn() {
+//     console.log("Hello from fn");
+// }
+// fn();
+// fn.prop = "property of function";
+// fn.myfn = function fn() {
+//     console.log("I am a method of function");
+// }
+// console.log(fn);
+// fn();
+// fn.myfn();
+// console.log(fn.prop);
+
+// JS ->primitive or object
+// everything object -> Dates, errors, modules
+// 6 primitive types -> number, string, boolean, undefined, null, symbol
