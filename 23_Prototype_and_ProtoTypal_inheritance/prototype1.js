d114790d66ab3cd44a906bc77055c32b0e6521b2
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-01-10 15:04:38 +0530

    Create: 23_Prototype_and_ProtoTypal_inheritance/

---
+++ 23_Prototype_and_ProtoTypal_inheritance/prototype1.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 4, 5];
+
+
+const person = {
+    name: "Steve",
+    age: 25,
+
+    showDetails: function(){
+        console.log(this.name, this.age);
+    }
+}
---
+++ 23_Prototype_and_ProtoTypal_inheritance/prototype2.js
@@ -0,0 +1,16 @@
+const person1 = {
+  name: "Adam",
+  age: 25,
+
+  showDetails: function () {
+    console.log(this.name + " " + this.age);
+  },
+};
+
+const person2 = {
+  name: "Steve",
+};
+
+person2.__proto__ = person1;
+console.log(person2.name + " " + person2.age);
+person2.showDetails()
