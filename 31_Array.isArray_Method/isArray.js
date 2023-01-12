9c621d662c40f66fc23d865595e9ca9c56ac3efc
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-01-12 14:32:52 +0530

    Create:  31_Array.isArray_Method/

---
+++ 31_Array.isArray_Method/isArray.js
@@ -0,0 +1,21 @@
+let arr = [1, 2, 3, 4, 5];
+
+
+let str = 'Hello';
+
+let obj = {
+    name: "Adam",
+    age: 23,
+};
+
+let num = 5;
+
+// // typeof
+// console.log(typeof str);
+// console.log(typeof obj);
+// console.log(typeof num);
+// console.log(typeof arr);
+
+// Array.isArray()
+let test = Array.isArray(str);
+console.log(test);
