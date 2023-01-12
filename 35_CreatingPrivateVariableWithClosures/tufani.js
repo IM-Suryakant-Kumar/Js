7898b45f00eb763b1f48d616c8704c76a2a91e18
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-01-12 17:33:22 +0530

    Create: 35_CreatingPrivateVariableWithClosures/

---
+++ 35_CreatingPrivateVariableWithClosures/tufani.js
@@ -0,0 +1,21 @@
+let myFunction = (function () {
+  let name = "";  // Private
+
+  getName = function () {
+    return name;
+  };
+
+  setName = function (newName) {
+    name = newName;
+  };
+
+  return {
+    getName: getName,
+    setName: setName,
+  };
+})();
+
+// myFunction.setName("Martin");
+// console.log(myFunction.getName())
+
+console.log(myFunction.name);
