68c4e70ee0a77f2f54ce532196227d48ba32e27f
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-24 16:45:46 +0530

    practice: http module

--- 39_Node_Module_Practice/app.js
+++ 39_Node_Module_Practice/app.js
@@ -59,15 +59,34 @@
 // emitter.emit("logging", { id: 2, message: "Raise logging!" });
 
 // --------------------------------------
-const EventEmitter = require("events");
+// const EventEmitter = require("events");
 
-const Logger = require("./logger");
-const logger = new Logger();
+// const Logger = require("./logger");
+// const logger = new Logger();
 
-// Register a listener
-logger.on("messagedLogged", (arg) => {  // e, eventArg
-  console.log("Listener Called", arg);
+// // Register a listener
+// logger.on("messagedLogged", (arg) => {  // e, eventArg
+//   console.log("Listener Called", arg);
+// });
+
+// logger.log("message");
+
+// ------------------------------------
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if(req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+
+  if(req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log("message");
+server.listen(3000);
+
+console.log("Listening on port 3000...");
 
