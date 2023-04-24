a764d0c1cdbf5a37765be42b947f71b4a274eb7e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-24 16:05:15 +0530

    practice: upto challenge: Raise: logging (data: message)

---
+++ 39_Node_Module_Practice/app.js
@@ -0,0 +1,59 @@
+// const path = require("path");
+
+// let pathObj = path.parse(__filename);
+// console.log(pathObj);
+
+// -----------------------------
+// const os = require("os");
+
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
+
+// // console.log("Total Memory: " + totalMemory);
+
+// // Template strings
+// // ES6 / ES2015 : ECMAScript 6
+
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+// --------------------------------
+// const fs = require("fs");
+
+// const files = fs.readdirSync("./");
+// console.log(files);
+
+// fs.readdir("./", (err, files) => {
+//   if(err) console.log("Error", err);
+//   else console.log("Result", files);
+
+// fs.readdir("$", (err, files) => {
+//   if(err) console.log("Error", err);
+//   else console.log("Result", files);
+// });
+
+// --------------------------------------
+// const EventEmitter =  require("events");
+// const emitter = new EventEmitter();
+
+// // Register a listener
+// emitter.on("messagedLogged", (arg) => {  // e, eventArg
+//   console.log("Listener Called", arg);
+// });
+
+// // Raise an event
+// emitter.emit("messagedLogged", { id: 1, url: "http://" });
+// // Making a noise, produce - signaling
+
+// Challenge ---------------------------------------
+// Raise: logging (data: message)
+// const EventEmitter = require("events");
+// const emitter = new EventEmitter();
+
+// // Register an event
+// emitter.on("logging", (arg) => {
+//   console.log(arg.message);
+// })
+
+// // Raise listener
+// emitter.emit("logging", { id: 2, message: "Raise logging!" });
